17dcb7cd918967960293c0a8e4da12c47358fe14
nishibayashi sakine <s.n.baton319@icloud.com>
2017-06-25 22:32:06 +0900

    6/24

--- heal/AddDiaryViewController.swift
+++ heal/AddDiaryViewController.swift
@@ -8,17 +8,39 @@
 
 import UIKit
 
-class AddDiaryViewController: UIViewController {
+class AddDiaryViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        scrollView.delegate = self
+        scrollView.indicatorStyle = .white
+        scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        scrollView.contentSize = CGSize(width: 343, height: 1000)
+        let view: UIView = UIView()
+        view.frame = CGRect(x:16,y:25,width:343,height:1000)
+        view.cornerRadius = 20
+        view.backgroundColor = UIColor.red
+        scrollView.addSubview(view)
         // Do any additional setup after loading the view.
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // スクロール中の処理
+        print("didScroll")
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        // ドラッグ開始時の処理
+        print("beginDragging")
     }
     
 
