e8aa0bc8c86bf6201bb968baa28ca725c6912eb4
nishibayashi sakine <s.n.baton319@icloud.com>
2017-12-13 20:37:25 +0900

    wip #29

---
+++ heal/SummeryRecordCell.swift
@@ -0,0 +1,21 @@
+//
+//  SummeryRecordCell.swift
+//  heal
+//
+//  Created by 西林咲音 on 2017/09/20.
+//  Copyright © 2017年 西林咲音. All rights reserved.
+//
+
+import UIKit
+
+class SummeryRecordCell: UICollectionViewCell {
+    
+    @IBOutlet var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
+
